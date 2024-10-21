16b35f89b85d4610da872213d8e5ce591bf37250
김희망 <gmlakd990514@naver.com>
2024-10-21 09:37:08 +0900

    init: init

---
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/SsacthreeFrontApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.mini_dooray.ssacthree_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsacthreeFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsacthreeFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/mini_dooray/ssacthree_front/SsacthreeFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.mini_dooray.ssacthree_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsacthreeFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
