c1af85c8b99a0d1872383e7d6bf9ddb06e49338a
김희망 <gmlakd990514@naver.com>
2024-10-21 15:50:06 +0900

    test: home 컨트롤러 추가

---
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.mini_dooray.ssacthree_front.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
