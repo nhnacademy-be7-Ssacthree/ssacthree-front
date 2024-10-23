94a7d07b063ffad7ced529dc40c73d4943d86f76
김희망 <gmlakd990514@naver.com>
2024-10-23 23:37:43 +0900

    feat: commons adapter 추가

---
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/controller/adapter/Adapter.java
@@ -0,0 +1,48 @@
+package com.nhnacademy.mini_dooray.ssacthree_front.controller.adapter;
+
+import java.util.List;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@RequiredArgsConstructor
+@Component
+public class Adapter {
+
+    private final RestTemplate restTemplate;
+
+    public <T> ResponseEntity<T> get(String url, Class<T> responseType) {
+        return restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+    }
+
+    public <T> ResponseEntity<List<T>> getList(String url, ParameterizedTypeReference<List<T>> responseType) {
+        return restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+    }
+
+    public ResponseEntity<MessageDto> post(String url, Object request) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Object> entity = new HttpEntity<>(request, httpHeaders);
+
+        return restTemplate.exchange(url, HttpMethod.POST, entity, MessageDto.class);
+    }
+
+    public ResponseEntity<MessageDto> put(String url, Object request) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Object> entity = new HttpEntity<>(request, httpHeaders);
+
+        return restTemplate.exchange(url, HttpMethod.PUT, entity, MessageDto.class);
+    }
+
+    public ResponseEntity<MessageDto> delete(String url) {
+        return restTemplate.exchange(url, HttpMethod.DELETE, null, MessageDto.class);
+    }
+}
--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/controller/MemberRegisterController.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/controller/MemberRegisterController.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.mini_dooray.ssacthree_front.member.controller;
 
+import com.nhnacademy.mini_dooray.ssacthree_front.member.dto.MemberRegisterRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
