bb66f82dc6cac66fb934b3573619d29583a3dc3d
김희망 <gmlakd990514@naver.com>
2024-10-25 20:20:32 +0900

    feat: 회원가입 시 ModelAttribute 유효성 검증

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/controller/MemberRegisterController.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/controller/MemberRegisterController.java
@@ -1,10 +1,13 @@
 package com.nhnacademy.mini_dooray.ssacthree_front.member.controller;
 
+import com.nhnacademy.mini_dooray.ssacthree_front.commons.dto.exception.ValidationFailedException;
 import com.nhnacademy.mini_dooray.ssacthree_front.member.dto.MemberRegisterRequest;
 import com.nhnacademy.mini_dooray.ssacthree_front.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +24,11 @@ public class MemberRegisterController {
     }
 
     @PostMapping("/register")
-    public String memberRegister(@ModelAttribute MemberRegisterRequest memberRegisterRequest, Model model) {
+    public String memberRegister(@Valid @ModelAttribute MemberRegisterRequest memberRegisterRequest,
+        BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            throw new ValidationFailedException(bindingResult);
+        }
         memberService.memberRegister(memberRegisterRequest);
         return "redirect:/";
     }
--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/dto/MemberRegisterRequest.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/dto/MemberRegisterRequest.java
@@ -19,10 +19,27 @@ public class MemberRegisterRequest {
     @NotBlank
     @Size(min = 5, max = 20)
     private String loginId;
+
+    @NotBlank
+    @Size(min = 8, max = 20)
+    //비밀번호는 8글자에서 20글자만 받자!
     private String loginPassword;
+
+    @NotBlank
+    @Size(max = 10)
+    // 10글자만 주기로 함.
     private String customerName;
+
+    @NotBlank
+    @Size(max = 13)
     private String customerPhoneNumber;
+
+    @NotBlank
+    @Size(max = 50)
     private String customerEmail;
+
+    @NotBlank
+    @Size(max = 8)
     private String birth;
 
 }
