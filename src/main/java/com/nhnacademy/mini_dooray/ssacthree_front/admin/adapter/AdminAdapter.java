e33d117898f54ccaa587e1c37f3826075cc1e008
Lee Eo Jin <win929@naver.com>
2024-10-31 18:33:57 +0900

    feat: 배송 정책 선택 구현

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/admin/adapter/AdminAdapter.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/admin/adapter/AdminAdapter.java
@@ -2,6 +2,7 @@ package com.nhnacademy.mini_dooray.ssacthree_front.admin.adapter;
 
 import com.nhnacademy.mini_dooray.ssacthree_front.admin.dto.DeliveryRuleCreateRequest;
 import com.nhnacademy.mini_dooray.ssacthree_front.admin.dto.DeliveryRuleGetResponse;
+import com.nhnacademy.mini_dooray.ssacthree_front.admin.dto.DeliveryRuleUpdateRequest;
 import com.nhnacademy.mini_dooray.ssacthree_front.commons.dto.MessageResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name="adminSendClient", url = "${admin-client.url}")
+@FeignClient(name="gateway-service", url = "${admin-client.url}")
 public interface AdminAdapter {
-
     @GetMapping("/deliveryRules")
     ResponseEntity<List<DeliveryRuleGetResponse>> getAllDeliveryRules();
 
     @PostMapping("/deliveryRules/create")
     ResponseEntity<MessageResponse> createDeliveryRule(@RequestBody DeliveryRuleCreateRequest deliveryRuleCreateRequest);
+
+    @PostMapping("/deliveryRules/update")
+    ResponseEntity<MessageResponse> updateDeliveryRule(@RequestBody DeliveryRuleUpdateRequest deliveryRuleUpdateRequest);
 }
