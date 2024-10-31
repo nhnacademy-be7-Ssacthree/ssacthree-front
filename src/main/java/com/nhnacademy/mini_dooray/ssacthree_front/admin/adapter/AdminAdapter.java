603b0107413d3a0a8ad062e244a570b894cb5d00
김희망 <gmlakd990514@naver.com>
2024-10-31 13:40:45 +0900

    fix: feignclient gateway랑 연결

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/admin/adapter/AdminAdapter.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/admin/adapter/AdminAdapter.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name="adminSendClient", url = "${admin-client.url}")
+@FeignClient(name="gateway-service", url = "${admin-client.url}")
 public interface AdminAdapter {
 
     @GetMapping("/deliveryRules")
--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/controller/MemberLoginController.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/controller/MemberLoginController.java
@@ -1,11 +1,10 @@
 package com.nhnacademy.mini_dooray.ssacthree_front.member.controller;
 
-import com.nhnacademy.mini_dooray.ssacthree_front.commons.dto.MessageResponse;
 import com.nhnacademy.mini_dooray.ssacthree_front.member.dto.MemberLoginRequest;
 import com.nhnacademy.mini_dooray.ssacthree_front.member.service.MemberService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
