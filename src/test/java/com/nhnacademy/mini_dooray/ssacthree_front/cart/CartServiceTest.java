e069b6e033cc2842c4f9a0dfa6667aff6f2ec39b
조병현 <jbk28252@naver.com>
2024-10-31 18:02:07 +0900

    fix : 테스트 수정

--- src/test/java/com/nhnacademy/mini_dooray/ssacthree_front/cart/CartServiceTest.java
+++ src/test/java/com/nhnacademy/mini_dooray/ssacthree_front/cart/CartServiceTest.java
@@ -6,10 +6,12 @@ import jakarta.servlet.http.HttpSession;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -22,7 +24,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class CartServiceTest {
 
     @Mock
