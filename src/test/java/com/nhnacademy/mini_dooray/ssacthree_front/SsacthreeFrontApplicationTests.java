360edd454e3334b4963e94a97cc6aaeca2019ecc
조병현 <jbk28252@naver.com>
2024-10-31 17:51:38 +0900

    fix : 테스트 수정

--- src/test/java/com/nhnacademy/mini_dooray/ssacthree_front/SsacthreeFrontApplicationTests.java
+++ src/test/java/com/nhnacademy/mini_dooray/ssacthree_front/SsacthreeFrontApplicationTests.java
@@ -1,13 +1,21 @@
 package com.nhnacademy.mini_dooray.ssacthree_front;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SsacthreeFrontApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        // 애플리케이션 컨텍스트가 정상적으로 로드되는지 확인
+        assertThat(applicationContext).isNotNull();
     }
-
 }
