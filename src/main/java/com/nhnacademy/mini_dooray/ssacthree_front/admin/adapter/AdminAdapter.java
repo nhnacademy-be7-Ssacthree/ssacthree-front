03f308dc1252fc373ce52198e1e1019ebf015afb
Lee Eo Jin <win929@naver.com>
2024-11-04 17:29:11 +0900

    fix: REST API 형태로 수정

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/admin/adapter/AdminAdapter.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/admin/adapter/AdminAdapter.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -21,6 +22,6 @@ public interface AdminAdapter {
     @PostMapping("/deliveryRules/create")
     ResponseEntity<MessageResponse> createDeliveryRule(@RequestBody DeliveryRuleCreateRequest deliveryRuleCreateRequest);
 
-    @PostMapping("/deliveryRules/update")
+    @PutMapping("/deliveryRules")
     ResponseEntity<MessageResponse> updateDeliveryRule(@RequestBody DeliveryRuleUpdateRequest deliveryRuleUpdateRequest);
 }
--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/admin/controller/AdminController.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/admin/controller/AdminController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -54,7 +51,7 @@ public class AdminController {
         return "redirect:/admin/deliveryRules";
     }
 
-    @PostMapping("/deliveryRules/update")
+    @PutMapping("/deliveryRules")
     public String updateDeliveryRule(@Valid @ModelAttribute DeliveryRuleUpdateRequest deliveryRuleUpdateRequest,
                                      BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
