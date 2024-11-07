109e66944d0cfac1287fc05483ced5c86cf6565e
hoohsh <hoohsh@daum.net>
2024-11-07 13:32:05 +0900

    fix: 카테고리 어댑터 url 변경, properties url 추가

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/bookset/category/adapter/CategoryAdapter.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/bookset/category/adapter/CategoryAdapter.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name="gateway-service", url = "http://localhost:8081/api/shop", contextId = "categoryClient")
+@FeignClient(name="gateway-service", url = "shop.url", contextId = "categoryClient")
 public interface CategoryAdapter {
 
     /**
