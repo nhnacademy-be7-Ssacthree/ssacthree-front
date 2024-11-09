85640edfbe331023dd2a9352c9e76887fb40410b
조병현 <jbk28252@naver.com>
2024-11-09 15:26:48 +0900

    feat : 테스트 추가

---
+++ src/test/java/com/nhnacademy/mini_dooray/ssacthree_front/address/controller/AddressServiceController.java
@@ -0,0 +1,5 @@
+package com.nhnacademy.mini_dooray.ssacthree_front.address.controller;
+
+public class AddressServiceController {
+
+}
---
+++ src/test/java/com/nhnacademy/mini_dooray/ssacthree_front/address/service/AddressServiceTest.java
@@ -0,0 +1,5 @@
+package com.nhnacademy.mini_dooray.ssacthree_front.address.service;
+
+public class AddressServiceTest {
+
+}
