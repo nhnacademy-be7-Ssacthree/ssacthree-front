9bbe43192788c43d7a118ad700d920a10e9a2032
hoohsh <hoohsh@daum.net>
2024-11-11 14:15:40 +0900

    feat: book front 구현 클래스 생성, 관련 dto 추가, fix: 테스트 오류로 패키징 불가해서 주석처리

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/bookset/tag/dto/TagInfoResponse.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/bookset/tag/dto/TagInfoResponse.java
@@ -1,11 +1,13 @@
 package com.nhnacademy.mini_dooray.ssacthree_front.bookset.tag.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @Getter
+@AllArgsConstructor // 써야 할 일이 있을 것 같아서 추가
+@NoArgsConstructor
 public class TagInfoResponse {
-
+    Long tagId;
     String tagName;
 }
