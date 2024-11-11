746485af41ae72069c08ceab5da6f0651efba80c
조병현 <jbk28252@naver.com>
2024-11-11 22:54:34 +0900

    feat : u 완성

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/cart/adapter/CartAdapter.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/cart/adapter/CartAdapter.java
@@ -1,11 +1,14 @@
 package com.nhnacademy.mini_dooray.ssacthree_front.cart.adapter;
 
 import com.nhnacademy.mini_dooray.ssacthree_front.cart.domain.CartItem;
+import com.nhnacademy.mini_dooray.ssacthree_front.cart.dto.CartRequest;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "gateway-service", url = "${member.url}", contextId = "CartClient")
@@ -16,4 +19,7 @@ public interface CartAdapter {
 
     @GetMapping("/shop/{bookId}")
     ResponseEntity<CartItem> getRandomBook(@RequestHeader("Authorization") String authorizationHeader, @PathVariable("bookId") Long bookId);
+
+    @PutMapping("/shop") // 만들어야함
+    ResponseEntity<Void> saveCart(@RequestHeader("Authorization") String authorizationHeader, @RequestBody List<CartRequest> cartList);
 }
