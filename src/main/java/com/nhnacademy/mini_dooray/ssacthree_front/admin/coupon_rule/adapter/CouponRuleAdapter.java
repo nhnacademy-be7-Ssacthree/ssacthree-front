6b0cd18047e7eb30fb4ce20e13fe9a683699ae1c
Lee Eo Jin <win929@naver.com>
2024-11-13 16:12:18 +0900

    feat: 쿠폰 정책 CRUD 구현 완료

---
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/admin/coupon_rule/adapter/CouponRuleAdapter.java
@@ -0,0 +1,26 @@
+package com.nhnacademy.mini_dooray.ssacthree_front.admin.coupon_rule.adapter;
+
+import com.nhnacademy.mini_dooray.ssacthree_front.admin.coupon_rule.dto.CouponRuleCreateRequest;
+import com.nhnacademy.mini_dooray.ssacthree_front.admin.coupon_rule.dto.CouponRuleGetResponse;
+import com.nhnacademy.mini_dooray.ssacthree_front.admin.coupon_rule.dto.CouponRuleUpdateRequest;
+import com.nhnacademy.mini_dooray.ssacthree_front.commons.dto.MessageResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name="gateway-service", url = "${admin-client.url}", contextId = "couponRuleClient")
+public interface CouponRuleAdapter {
+    @GetMapping("/coupon-rules")
+    ResponseEntity<List<CouponRuleGetResponse>> getAllCouponRules();
+
+    @PutMapping("/coupon-rules")
+    ResponseEntity<MessageResponse> updateCouponRule(@RequestBody CouponRuleUpdateRequest couponRuleUpdateRequest);
+
+    @PostMapping("/coupon-rules")
+    ResponseEntity<MessageResponse> createCouponRule(@RequestBody CouponRuleCreateRequest couponRuleCreateRequest);
+}
