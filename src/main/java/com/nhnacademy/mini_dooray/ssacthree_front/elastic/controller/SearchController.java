3c426f7f84c8d73f475588e80662677af0c364f7
jhw <wjdg159@gmail.com>
2024-11-14 22:58:07 +0900

    feat:키워드 검색 구현

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/elastic/controller/SearchController.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/elastic/controller/SearchController.java
@@ -1,19 +1,19 @@
 package com.nhnacademy.mini_dooray.ssacthree_front.elastic.controller;
 
 import com.nhnacademy.mini_dooray.ssacthree_front.elastic.domain.BookDocument;
-import com.nhnacademy.mini_dooray.ssacthree_front.elastic.dto.SearchRequest;
 import com.nhnacademy.mini_dooray.ssacthree_front.elastic.service.SearchService;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/search")
@@ -29,12 +29,13 @@ public class SearchController {
 
   @GetMapping("/books")
   public String searchBooks(@RequestParam String keyword,
-              @RequestParam(defaultValue = "1") int page,
-              @RequestParam(required = false) String sort,
-              @RequestParam(required = false) String category,
-              @RequestParam(required = false) String tag,
-              Model model) {
-
+                            @RequestParam(defaultValue = "1") int page,
+                            @RequestParam(required = false) String sort,
+                            @RequestParam(required = false, defaultValue = "20") int pageSize,
+                            @RequestParam(required = false) String category,
+                            @RequestParam(required = false) String tag,
+                            Model model) {
+    log.info("검색을 시도합니다. {}", keyword);
     Map<String, String> filters = new HashMap<>();
     if (category != null) {
       filters.put("category", category);
@@ -43,8 +44,8 @@ public class SearchController {
       filters.put("tag", tag);
     }
     // SearchService를 통해 백엔드로 데이터 전달 후 결과 받기
-    List<BookDocument> books = searchService.searchBooks(keyword, page, sort, filters);
-
+    List<BookDocument> books = searchService.searchBooks(keyword, page, sort, pageSize, filters);
+    log.info("books 리스트를 전달 받았습니다. {}" ,books);
     model.addAttribute("books", books);  // 결과를 Model에 담아 view로 전달
     return "books";  // books.html로 결과 전달
   }
--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/elastic/service/SearchService.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/elastic/service/SearchService.java
@@ -2,7 +2,6 @@ package com.nhnacademy.mini_dooray.ssacthree_front.elastic.service;
 
 import com.nhnacademy.mini_dooray.ssacthree_front.elastic.adapter.SearchAdapter;
 import com.nhnacademy.mini_dooray.ssacthree_front.elastic.domain.BookDocument;
-import com.nhnacademy.mini_dooray.ssacthree_front.elastic.dto.SearchRequest;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,13 +14,13 @@ public class SearchService {
   private final SearchAdapter searchAdapter;
 
 
-  public List<BookDocument> searchBooks(String keyword, int page, String sort, Map<String, String> filters) {
+  public List<BookDocument> searchBooks(String keyword, int page, String sort, int pageSize, Map<String, String> filters) {
     // 전달 받은 값들을 사용해 쿼리 작성 및 API 호출
-//    SearchRequest searchRequest = new SearchRequest(keyword, page, sort, filters);
+
     // filters 맵에서 category와 tag 값을 가져와 개별 파라미터로 전달
     String category = filters.get("category");
     String tag = filters.get("tag");
-    return searchAdapter.searchBooks(keyword, page, sort, category, tag);
+    return searchAdapter.searchBooks(keyword, page, sort, pageSize, category, tag);
   }
 
 }
