9e086ddf8ad2e9c452731cb86a4a5974ad8cae5f
김희망 <gmlakd990514@naver.com>
2024-11-15 02:02:19 +0900

    feat: 회원가입 유효성 검증 및 url 문제 수정

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/commons/interceptor/CheckLoginInterceptor.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/commons/interceptor/CheckLoginInterceptor.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 로그인을 체크하기위한 interceptor - (FE header)
+ *
  * @author : 김희망
  * @Date : 2024/11/03
  */
@@ -18,16 +19,16 @@ public class CheckLoginInterceptor implements HandlerInterceptor {
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
         ModelAndView modelAndView) throws Exception {
 
-        if(modelAndView != null) {
+        if (modelAndView != null) {
             boolean accessTokenExists = false;
-            if(request.getCookies() != null) {
-                for(Cookie cookie : request.getCookies()) {
-                    if(cookie.getName().equals("access-token")) {
+            if (request.getCookies() != null) {
+                for (Cookie cookie : request.getCookies()) {
+                    if (cookie.getName().equals("access-token")) {
                         accessTokenExists = true;
                     }
                 }
             }
-            modelAndView.addObject("accessTokenExists", accessTokenExists);
+            request.setAttribute("accessTokenExists", accessTokenExists);
         }
     }
 }
--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/dto/MemberRegisterRequest.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/dto/MemberRegisterRequest.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.mini_dooray.ssacthree_front.member.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,30 +15,40 @@ import lombok.Setter;
 public class MemberRegisterRequest {
 
 
-    @NotBlank
-    @Size(min = 5, max = 20)
+    @NotBlank(message = "공백일 수 없습니다.")
+    @Size(min = 5, max = 20, message = "5~20글자 사이로 입력해야합니다.")
+    @Pattern(regexp = "^[A-Za-z0-9]{5,20}$", message = "아이디는 영어와 숫자로 5자에서 20자 사이로 입력하십시오.")
     private String loginId;
 
-    @NotBlank
-    @Size(min = 8, max = 20)
+    @NotBlank(message = "공백일 수 없습니다.")
+    @Size(min = 8, max = 20, message = "8~20글자 사이로 입력해야합니다.")
     //비밀번호는 8글자에서 20글자만 받자!
     private String loginPassword;
 
-    @NotBlank
-    @Size(max = 10)
+    @NotBlank(message = "공백일 수 없습니다.")
+    @Size(max = 10, message = "최대 10글자만 입력할 수 있습니다.")
     // 10글자만 주기로 함.
     private String customerName;
 
-    @NotBlank
-    @Size(max = 13)
+    @NotBlank(message = "공백일 수 없습니다.")
+    @Size(max = 11)
+    @Pattern(regexp = "^\\d{11}$", message = "전화번호는 하이픈 없이 11자리 숫자여야 합니다.")
     private String customerPhoneNumber;
 
-    @NotBlank
+    @NotBlank(message = "공백일 수 없습니다.")
     @Size(max = 50)
+    @Pattern(regexp = "^[A-Za-z0-9_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", message = "올바른 이메일 주소를 입력하세요.")
     private String customerEmail;
 
-    @NotBlank
+    @NotBlank(message = "공백일 수 없습니다.")
     @Size(max = 8)
     private String birth;
+
+    public void setBirth(String birth) {
+        if (birth != null) {
+            this.birth = birth.replace("-", "");
+        }
+    }
+
 
 }
