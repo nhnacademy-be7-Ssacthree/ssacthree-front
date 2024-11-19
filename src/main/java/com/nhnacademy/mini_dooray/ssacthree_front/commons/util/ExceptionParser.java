777f8be3b9b9891ecb7eea14647a4e2b50dd719b
김희망 <gmlakd990514@naver.com>
2024-11-20 00:41:02 +0900

    fix: 에러 페이지 뷰 변경, FeignException에서 바디 추출 유틸클래스 구현, 기타 alter창

---
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/commons/util/ExceptionParser.java
@@ -0,0 +1,30 @@
+package com.nhnacademy.mini_dooray.ssacthree_front.commons.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.mini_dooray.ssacthree_front.commons.dto.ErrorResponse;
+
+
+public class ExceptionParser {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private ExceptionParser() {
+    }
+
+    /**
+     * FeignException의 메세지를 빼내는 메소드
+     *
+     * @param errorMessageBody
+     * @return
+     */
+    public static String getErrorMessageFromFeignException(String errorMessageBody) {
+
+        try {
+            return objectMapper.readValue(errorMessageBody, ErrorResponse.class).getMessage();
+        } catch (JsonProcessingException e) {
+            return errorMessageBody;
+        }
+
+    }
+}
