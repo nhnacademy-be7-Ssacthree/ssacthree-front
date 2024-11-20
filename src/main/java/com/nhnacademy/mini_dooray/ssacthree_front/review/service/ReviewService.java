c1fd6e41436f5dec89557a13721e7641d2b47ad2
조병현 <jbk28252@naver.com>
2024-11-20 20:31:23 +0900

    feat : 리뷰 저장 추가

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/review/service/ReviewService.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/review/service/ReviewService.java
@@ -1,10 +1,16 @@
 package com.nhnacademy.mini_dooray.ssacthree_front.review.service;
 
+import com.nhnacademy.mini_dooray.ssacthree_front.review.dto.ReviewRequest;
 import com.nhnacademy.mini_dooray.ssacthree_front.review.dto.ReviewResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public interface ReviewService {
 
     List<ReviewResponse> getReviewsByBookId(Long bookId);
+
+    void postReviewBook(Long bookId,Long orderId,ReviewRequest reviewRequest, HttpServletRequest request);
+
+    Long authToWriteReview(Long bookId, HttpServletRequest request);
 
 }
