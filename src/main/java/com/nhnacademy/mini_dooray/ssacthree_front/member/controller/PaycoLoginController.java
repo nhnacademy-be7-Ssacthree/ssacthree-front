4b0e4b4b2600e8b1286d5aa9624a5ca209e68383
김희망 <gmlakd990514@naver.com>
2024-11-21 17:36:13 +0900

    fix: prod properties 수정

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/controller/PaycoLoginController.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/member/controller/PaycoLoginController.java
@@ -3,11 +3,9 @@ package com.nhnacademy.mini_dooray.ssacthree_front.member.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping
 public class PaycoLoginController {
 
 
