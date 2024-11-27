d9298229d802b0743c89681c5bee607dd7db4d15
조병현 <jbk28252@naver.com>
2024-11-27 17:23:24 +0900

    fix : 리뷰 작성 오류 수정

---
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/review/exception/NoOrderException.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.mini_dooray.ssacthree_front.review.exception;
+
+public class NoOrderException extends RuntimeException {
+
+    public NoOrderException(String message) {
+        super(message);
+    }
+}
