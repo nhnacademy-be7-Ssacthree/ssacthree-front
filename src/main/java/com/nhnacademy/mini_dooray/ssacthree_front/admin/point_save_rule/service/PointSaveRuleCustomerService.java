cc4e7a7814ef441789cabf88c1c9afad9b830299
Huimang <83748099+gimalgosa0514@users.noreply.github.com>
2024-11-29 11:08:55 +0900

    Update PointSaveRuleCustomerService.java

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/admin/point_save_rule/service/PointSaveRuleCustomerService.java
+++ src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/admin/point_save_rule/service/PointSaveRuleCustomerService.java
@@ -6,7 +6,7 @@ public interface PointSaveRuleCustomerService {
 
     PointSaveRuleInfoResponse getPointSaveRuleByRuleName(String pointSaveRuleName);
 
-    ;
+    
 
     PointSaveRuleInfoResponse getBookPointSaveRule();
 }
