fd8c3a333e2291f0d8888d27908093b87f7f6201
jhw <wjdg159@gmail.com>
2024-12-05 13:05:18 +0900

    FailedCreateOrder 삭제 (사용안함)

--- src/main/java/com/nhnacademy/mini_dooray/ssacthree_front/order/exception/FailedCreateOrder.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.mini_dooray.ssacthree_front.order.exception;
-
-public class FailedCreateOrder extends RuntimeException {
-
-  public FailedCreateOrder(String message) {
-    super(message);
-  }
-}
